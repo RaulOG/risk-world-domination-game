7bf5a592aa716847ea4e2cb5b8cee544abaf178b
RaulOG <raulog1989@gmail.com>
2016-02-12 16:19:24 +0100

    Added font awesome to bower. Added font awesome and google fonts to the header

--- resources/views/layouts/partials/heads/head.blade.php
+++ resources/views/layouts/partials/heads/head.blade.php
@@ -1,2 +1,6 @@
 <title>@yield('title')</title>
 {{ HTML::style('bower/bootstrap/dist/css/bootstrap.min.css') }}
+
+<!-- Fonts -->
+{{ HTML::style('bower/fontawesome/css/font-awesome.min.css') }}
+<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
