b448b02537cb8eba9fce362d0a8bf59ec76b3b3d
HectorOrdonez <hjavierog@gmail.com>
2016-02-17 17:03:25 +0100

    Game in progress layout

--- resources/views/games/partials/game-inprogress.blade.php
+++ resources/views/games/partials/game-inprogress.blade.php
@@ -6,4 +6,26 @@
         <h2>Game in progress</h2>
     </div>
 
+    <div>
+        <h2>
+            Go for it, {{ Auth::user()->name }}!
+        </h2>
+        <div class="container row">
+            <div class="col-md-6">
+                <p>{{ Auth::user()->name }}</p>
+                <div class="player-host">
+                    <p>number of troops</p>
+                    <h4>5</h4>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <p>your opponent</p>
+                <div class="player-guest">
+                    <p>number of troops</p>
+                    <h4>5</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
